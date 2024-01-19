b587913ac941e80b8872bc3eb37e175540378533
Yuen109 <ansonyuen109@gmail.com>
2024-01-19 13:41:15 +0800

    Modify motion of the <p>

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -19,9 +19,14 @@ const About = () => {
         src="https://media.licdn.com/dms/image/C5603AQEJfvf7gJ89Eg/profile-displayphoto-shrink_200_200/0/1654998888737?e=1710979200&v=beta&t=Jdo5xHdxeKb6PPLcVfepjeQXvIHrXr5WhhMoj5VK10g"
         className="w-40 h-40 rounded-full flex-shrink-0 object-cover md:rounded-lg md:w-64 md:h-64"
       />
-      <div className="space-y-10 px-0 py-4 md:px-10">
+      <motion.div
+        initial={{ x: 200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className="space-y-10 px-0 py-4 md:px-10"
+      >
         <h4 className="">Here is my background</h4>
-        <p className="text-xs md:w-60 ">
+        <p className="text-xs md:w-70">
           Hello and welcome to my website! My name is Anson and I am a web
           developer from Hong Kong. I have a passion for coding, reading books,
           and doing sports. I enjoy creating websites that are user-friendly,
@@ -32,7 +37,7 @@ const About = () => {
           interesting. Feel free to contact me if you have any questions or
           feedback. Thank you for visiting!
         </p>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
